eef5a77c8f71349744cc5d57483fc841be924217
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-10-24 14:30:00 -0700

    [DEV] updating package and addig veevalidate module

---
+++ src/plugin/types/vee-validate.d.ts
@@ -0,0 +1,19 @@
+declare module 'vee-validate' {
+	import {
+		Field,
+		FieldValidator,
+		Form,
+		FormValidationResult,
+		GenericObject,
+		PrivateFormContext,
+	} from 'vee-validate';
+
+	export {
+		Field,
+		FieldValidator,
+		Form,
+		FormValidationResult,
+		GenericObject,
+		PrivateFormContext
+	};
+}
