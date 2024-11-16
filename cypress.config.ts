77ddc7ad25ff4e348f056b0522401594ffb96bed
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-11-15 20:09:21 -0800

    adding and updating configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from 'cypress';
+import customViteConfig from './vite.cypress.config.ts';
 
 export default defineConfig({
 	e2e: {
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			config.env = {
+				...process.env,
+				...config.env,
+			};
+			return config;
 		},
+		specPattern: './src/**/*.spec.cy.{js,jsx,ts,tsx}',
 	},
 
 	component: {
 		devServer: {
 			bundler: 'vite',
 			framework: 'vue',
+			viteConfig: customViteConfig,
 		},
+		specPattern: './src/**/*.cy.{js,jsx,ts,tsx}',
 		viewportHeight: 800,
 		viewportWidth: 1920,
 	},
