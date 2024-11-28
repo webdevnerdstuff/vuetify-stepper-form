9e0dbb5a5a94c68cef9802397b5590223416263f
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-11-28 15:42:29 -0800

    updated desc

--- src/documentation/sections/DescriptionSection.vue
+++ src/documentation/sections/DescriptionSection.vue
@@ -14,7 +14,9 @@
 		</v-col>
 
 		<v-col cols="12">
-			Vuetify Stepper Form
+			The Vuetify Stepper Form plugin provides a structured way to create multi-step forms using Vue 3, TypeScript, and
+			Vuetify. It features a stepper layout that allows users to navigate between steps with form validation. The plugin
+			is customizable and streamlines building dynamic, interactive forms that guide users through sequential steps.
 		</v-col>
 
 	</v-row>
